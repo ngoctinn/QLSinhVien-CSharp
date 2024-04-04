a9ac770a2480c9ebc2d13875fa6206c0313595bb
ngoctinn <3121410041@sv.sgu.edu.vn>
2024-04-04 14:16:10 +0700

    Add project files.

---
+++ QLSinhVien-CSharp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace QLSinhVien_CSharp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
